fb7a8d1ccc246874eb831c61cbba47499ae56ed2
Dhiraj Salunke <dhirajraje113@gmail.com>
2021-11-10 10:46:48 +0530

    final submission.

--- server.js
+++ server.js
@@ -1,37 +1,14 @@
 const express = require('express');
+var cors = require('cors')
 require('dotenv').config();
 const dbConnect = require('./services/dbconnect');
 const routes = require('./Routes/routes')
 const app = express();
+app.use(cors())
+
 app.use(express.urlencoded({ extended: false }))
 
 app.use('/',routes)
-
-
-
-var data = require('./data.json')
-
-
-app.get('/', (req, res) => {
-    // req params pageNumber, pageSize
-    // res count, dreq.fileata
-    var pageNumber = req.query['pageNumber'] ?? 1;
-    var pageSize = req.query['pageSize'] ?? 10;
-    console.log(req.query);
-    if (pageNumber == 1) {
-        var dbData = data.slice(0, Math.min(data.length, pageSize));
-    } else {
-        console.log(pageSize * (pageNumber - 1));
-        console.log(pageSize * pageNumber);
-        var dbData = data.slice(pageSize * (pageNumber - 1), pageSize * pageNumber);
-    }
-    var resData = {
-        count: data.length,
-        data: dbData
-    }
-    res.send(resData)
-})
-
 
 dbConnect(process.env.DB_CONNECTION_STRING).then((data) => {
     console.log('Connected to db successfully')
